1d68f38b91e81cae5209ef80bc0e81ee76ad5389
Vitalii Pianykh <vitaliy.pianykh@gmail.com>
2026-06-18 06:46:18 -0400

    add initial set of files

---
+++ Sources/TimeKit/TimeError.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum TimeError: LocalizedError {
+    case generic(description: String)
+    
+    var errorDescription: String? {
+        switch self {
+        case .generic(let description):
+            description
+        }
+    }
+}
